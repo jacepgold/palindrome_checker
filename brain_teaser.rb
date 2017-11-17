722a1f88cece6c2deff0998f4d3de4d37e762fbe
Jace P. Gold <Jace@JaceGold.com>
2017-11-17 10:05:42 -0700

    Initial upload, DPL Week2 Brain Teaser

---
+++ brain_teaser.rb
@@ -0,0 +1,37 @@
+require 'colorize'
+
+@reversed = []
+@input_down
+@input = ''
+done = false
+
+
+def checker
+  count = 0
+  while count < @input_down.length
+    @reversed.push(@input_down[count])
+    puts "Reversed is #{@reversed[count]}"
+    count++
+  
+  if @input == @reversed
+    puts "#{@input} reversed is #{@reversed}"
+    puts "You have a Palindrome!".colorize(:green)
+  else
+    puts "No Match...".colorize(:red)
+  
+end
+
+
+while done == false
+  puts 'Enter a word'.colorize(:light_blue)
+  print '> '
+  @input = $stdin.gets.strip
+  @input_down = @input.downcase
+  @input.strip()
+  if @input_down == 'exit'
+    puts "Thanks for playing!"
+    done = true
+  end
+  checker
+  done = true
+end
